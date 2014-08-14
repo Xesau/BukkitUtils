1ccd2aba6141195e383296d8404ef71f0e032ba6
Xesau <ck-duinrand@zeelandnet.nl>
2014-08-14 22:42:36 +0200

    useArguments for ECommand
    Now you can use ECommand.addSubCommand( String, ECommand ) to add
    subCommands after creation. You can even use ECommand.addSubCommand( ...
    ).addSubCommand( ... ) as it is a builder.

--- src/mc/xesau/bukkitutils/command/ECommand.java
+++ src/mc/xesau/bukkitutils/command/ECommand.java
@@ -14,14 +14,19 @@ public class ECommand implements CommandExecutor {
 	private CommandExecutor mainExecutor;
 	private String helpMessage = "";
 	
+	private boolean useArguments = false;
+	
 	/**
 	 * Construct an ExtendedCommand
 	 * @param executor The CommandExecutor if there's no subcommand used
+	 * @param helpMessage The help message, for if there's a problem with the sub command structure
+	 * @param useArguments Whether the command should use arguments or sub commands
 	 */
-	public ECommand( CommandExecutor executor, String helpMessage )
+	public ECommand( CommandExecutor executor, String helpMessage, boolean useArguments )
 	{
 		this.mainExecutor = executor;
 		this.helpMessage = helpMessage;
+		this.useArguments = useArguments;
 	}
 	
 	/**
@@ -31,36 +36,42 @@ public class ECommand implements CommandExecutor {
 	 */	
 	public ECommand( CommandExecutor executor, String helpMessage, HashMap< String, ECommand > subCommands )
 	{
-		this( executor, helpMessage );
-		this.subCommands = subCommands;
+		this( executor, helpMessage, false );
+		
+		for( String subCommand : subCommands.keySet() )
+		{
+			this.subCommands.put( subCommand.toLowerCase(), subCommands.get( subCommand ) );
+		}
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if( args.length > 0 )
+		
+		if( useArguments )
 		{
-			if( subCommands.containsKey( args[0] ) )
+			if( args.length > 0 )
 			{
-				
-				// Create a List< String > with all the arguments for the sub command
-				ArrayList< String > subArguments = new ArrayList<String>( Arrays.asList( args ) );
-				
-				// Remove the sub command itself
-				subArguments.remove( 0 );
-				
-				// Run the command with the subArguments list converted back to an String[] array
-				return subCommands.get( args[0] ).onCommand(sender, command, label, (String[]) subArguments.toArray( new String[0]) );
-			}
-			else
-			{
-				sender.sendMessage( helpMessage );
-				return false;
+				if( subCommands.containsKey( args[0] ) )
+				{
+					
+					// Create a List< String > with all the arguments for the sub command
+					ArrayList< String > subArguments = new ArrayList<String>( Arrays.asList( args ) );
+					
+					// Remove the sub command itself
+					subArguments.remove( 0 );
+					
+					// Run the command with the subArguments list converted back to an String[] array
+					return subCommands.get( args[0] ).onCommand(sender, command, label, (String[]) subArguments.toArray( new String[0]) );
+				}
+				else
+				{
+					sender.sendMessage( helpMessage );
+					return false;
+				}
 			}
 		}
-		else
-		{
-			return mainExecutor.onCommand(sender, command, label, args);
-		}
+		
+		return mainExecutor.onCommand(sender, command, label, args);
 	}
 	
 	/**
@@ -81,4 +92,24 @@ public class ECommand implements CommandExecutor {
 		return subCommands;
 	}
 
+	/**
+	 * Add a sub command
+	 * 
+	 * @param name The name of the sub command. for example /test [name]
+	 * @param subcommand The ECommand instance that will be run when the subcommand is run
+	 * @throws IllegalStateException When useArguments (in the constructor without the subCommands HashMap) is true
+	 */
+	public ECommand addSubCommand( String name, ECommand subcommand ) throws IllegalStateException
+	{
+		if( !useArguments )
+		{
+			this.subCommands.put( name.toLowerCase(), subcommand );
+		}
+		else
+		{
+			throw new IllegalStateException( "Can't use subcommands when arguments are used" );
+		}
+		return this;
+	}
+	
 }
