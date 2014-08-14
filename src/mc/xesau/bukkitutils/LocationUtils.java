6f523fb1a4e0e77c77025061be2e991d09cb6ebd
Xesau <ck-duinrand@zeelandnet.nl>
2014-08-14 22:40:40 +0200

    Check for a location in a certain region.
    With this new update in LocationUtils.java, you can use isIn( Location
    check, Location point1, Location point2 ) to check if the location
    'check' is in the region of 'point1' and 'point2'

--- src/mc/xesau/bukkitutils/LocationUtils.java
+++ src/mc/xesau/bukkitutils/LocationUtils.java
@@ -14,4 +14,21 @@ public class LocationUtils {
 		);
 	}
 	
+	public static boolean isIn( Location check, Location point1, Location point2 )
+	{
+		return (
+			check.getWorld().getName() == point1.getWorld().getName() &&
+			check.getWorld().getName() == point2.getWorld().getName() &&
+		
+			check.getX() >= Math.min( point1.getX(), point2.getX() ) &&
+			Math.max( point1.getX(), point2.getX() ) >= check.getX() &&
+			
+			check.getY() >= Math.min( point1.getY(), point2.getY() ) &&
+			Math.max( point1.getY(), point2.getY() ) >= check.getY() &&
+			
+			check.getZ() >= Math.min( point1.getZ(), point2.getZ() ) &&
+			Math.max( point1.getZ(), point2.getZ() ) >= check.getZ()
+		);
+	}
+	
 }
